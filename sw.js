0c44e2551c6170ea06d7bd4bb367c101221e2ca0
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-03-16 10:25:06 +0530

    SW js updated

--- sw.js
+++ sw.js
@@ -1,11 +1,39 @@
+const CACHE_NAME = "civicpulse-v1";
+
 self.addEventListener("install", event => {
   console.log("Service Worker Installed");
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
